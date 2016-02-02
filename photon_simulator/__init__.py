73cd60b59cad1cc3058fad4920b3bac033ae3fb3
John ZuHone <jzuhone@gmail.com>
2016-02-02 14:47:22 -0500

    Rename this

---
+++ photon_simulator/__init__.py
@@ -0,0 +1,18 @@
+from photon_simulator.photon_models import \
+     PhotonModel, \
+     ThermalPhotonModel, \
+     LineEmissionModel, \
+     PowerLawModel
+
+from photon_simulator.main import \
+     PhotonList, \
+     EventList, \
+     merge_files, \
+     convert_old_file
+
+from photon_simulator.spectral_models import \
+     SpectralModel, \
+     XSpecThermalModel, \
+     XSpecAbsorbModel, \
+     TableApecModel, \
+     TableAbsorbModel
