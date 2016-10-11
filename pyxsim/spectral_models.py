b4c158c6b3087423fab71933917bc3d969979f8b
John ZuHone <jzuhone@gmail.com>
2016-10-11 10:45:27 -0400

    Docstrings. Simplifying WabsModel.

--- pyxsim/spectral_models.py
+++ pyxsim/spectral_models.py
@@ -304,6 +304,19 @@ class AbsorptionModel(SpectralModel):
         return np.exp(-self.sigma*self.nH)
 
     def absorb_photons(self, eobs, prng=np.random):
+        r"""
+        Determine which photons will be absorbed by foreground
+        galactic absorption.
+
+        Parameters
+        ----------
+        eobs : array_like
+            The energies of the photons in keV.
+        prng : :class:`~numpy.random.RandomState` object or :mod:`~numpy.random`, optional
+            A pseudo-random number generator. Typically will only be specified
+            if you have a reason to generate the same set of random numbers, such as for a
+            test. Default is the :mod:`numpy.random` module.
+        """
         mylog.info("Absorbing.")
         self.prepare_spectrum()
         aspec = self.get_spectrum()
@@ -416,6 +429,15 @@ class TBabsModel(TableAbsorbModel):
         super(TBabsModel, self).__init__("tbabs_table.h5", nH)
 
 
+emx = np.array([0.0, 0.1, 0.284, 0.4, 0.532, 0.707, 0.867,
+                1.303, 1.840, 2.471, 3.210, 4.038, 7.111, 8.331, 10.0])
+c0 = np.array([17.3, 34.6, 78.1, 71.4, 95.5, 308.9, 120.6, 141.3,
+               202.7,342.7,352.2,433.9,629.0,701.2])
+c1 = np.array([608.1, 267.9, 18.8, 66.8, 145.8, -380.6, 169.3,
+               146.8, 104.7, 18.7, 18.7, -2.4, 30.9, 25.2])
+c2 = np.array([-2150., -476.1 ,4.3, -51.4, -61.1, 294.0, -47.7,
+               -31.5, -17.0, 0.0, 0.0, 0.75, 0.0, 0.0])
+
 class WabsModel(AbsorptionModel):
     r"""
     Initialize a Wisconsin (Morrison and McCammon; ApJ 270, 119) 
@@ -425,27 +447,32 @@ class WabsModel(AbsorptionModel):
     ----------
     nH : float
         The foreground column density *nH* in units of 10^22 cm^{-2}.
-    emin : float, optional
-        The minimum energy for the spectral model.
-    emax : float, optional
-        The maximum energy for the spectral model.
-    nchan : integer, optional
-        The number of channels in the spectral model.
 
     Examples
     --------
     >>> wabs_model = WabsModel(0.1)
     """
-    def __init__(self, nH, emin=0.01, emax=50.0, nchan=100000):
+    def __init__(self, nH):
         self.nH = nH
-        super(WabsModel, self).__init__(emin, emax, nchan)
-        emx = np.array([0.0, 0.1, 0.284, 0.4, 0.532, 0.707, 0.867, 
-                        1.303, 1.840, 2.471, 3.210, 4.038, 7.111, 8.331, 10.0])
-        c0 = np.array([17.3, 34.6, 78.1, 71.4, 95.5, 308.9, 120.6, 141.3,
-                       202.7,342.7,352.2,433.9,629.0,701.2])
-        c1 = np.array([608.1, 267.9, 18.8, 66.8, 145.8, -380.6, 169.3,
-                       146.8, 104.7, 18.7, 18.7, -2.4, 30.9, 25.2])
-        c2 = np.array([-2150., -476.1 ,4.3, -51.4, -61.1, 294.0, -47.7,
-                       -31.5, -17.0, 0.0, 0.0, 0.75, 0.0, 0.0])
-        idxs = np.minimum(np.searchsorted(emx, self.emid)-1, 13)
-        self.sigma = (c0[idxs]+c1[idxs]*self.emid+c2[idxs]*self.emid)*1.0e-24/self.emid**3
+
+    def absorb_photons(self, eobs, prng=np.random):
+        r"""
+        Determine which photons will be absorbed by foreground
+        galactic absorption.
+
+        Parameters
+        ----------
+        eobs : array_like
+            The energies of the photons in keV.
+        prng : :class:`~numpy.random.RandomState` object or :mod:`~numpy.random`, optional
+            A pseudo-random number generator. Typically will only be specified
+            if you have a reason to generate the same set of random numbers, such as for a
+            test. Default is the :mod:`numpy.random` module.
+        """
+        mylog.info("Absorbing.")
+        idxs = np.minimum(np.searchsorted(emx, eobs)-1, 13)
+        sigma = (c0[idxs]+c1[idxs]*eobs+c2[idxs]*eobs)*1.0e-24/eobs**3
+        absorb = np.exp(-sigma*self.nH)
+        randvec = absorb.max()*prng.uniform(size=eobs.shape)
+        detected = randvec < absorb
+        return detected
