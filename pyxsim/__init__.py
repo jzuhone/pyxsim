774d86c8f5941e60137d380e2456a6132be35184
John ZuHone <jzuhone@gmail.com>
2017-01-05 12:08:51 -0500

    Be more committed about making dev version strings

--- pyxsim/__init__.py
+++ pyxsim/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.2.1"
+__version__ = "1.3-dev"
 
 from pyxsim.source_models import \
    SourceModel, \
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ cython_extensions = [
 
 setup(name='pyxsim',
       packages=['pyxsim'],
-      version='1.2.1',
+      version='1.3-dev',
       description='Python package for simulating X-ray observations of astrophysical sources',
       author='John ZuHone',
       author_email='jzuhone@gmail.com',
