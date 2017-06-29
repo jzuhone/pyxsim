be181767987586b3de0a48cde245e6a769ff6088
John ZuHone <jzuhone@gmail.com>
2017-06-29 15:54:38 -0400

    Make sure this happens for past versions of Python

--- pyxsim/source_generators/xray_binaries.py
+++ pyxsim/source_generators/xray_binaries.py
@@ -602,10 +602,11 @@ def make_xrb_particles(data_source, metallicity_field, age_field,
     # HACK: Something is funny with load_particles pre yt 3.4
     # and in Python 2 so we need to force particle types here 
     # for the moment
-    for field in ["particle_luminosity", "particle_count_rate",
-                  "particle_spectral_index"]:
-        fd = getattr(new_ds.fields.io, field)
-        setattr(fd, "particle_type", True)
+    if sys.version_info < (3, 6):
+        for field in ["particle_luminosity", "particle_count_rate",
+                      "particle_spectral_index"]:
+            fd = getattr(new_ds.fields.io, field)
+            setattr(fd, "particle_type", True)
 
     return new_ds
 
