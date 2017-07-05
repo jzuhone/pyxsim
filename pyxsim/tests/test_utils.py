f5aed4fafb390d8a71f210f31360f76ec05cb52d
John ZuHone <jzuhone@gmail.com>
2017-07-05 09:14:15 -0400

    A test for parse_value

---
+++ pyxsim/tests/test_utils.py
@@ -0,0 +1,13 @@
+from pyxsim.utils import parse_value
+from astropy.units import Quantity
+from yt import YTQuantity
+
+def test_parse_value():
+    t_yt = parse_value(YTQuantity(300.0, "ks"), "s")
+    t_astropy = parse_value(Quantity(300.0, "ks"), "s")
+    t_float = parse_value(300000.0, "s")
+    t_tuple = parse_value((300.0, "ks"), "s")
+
+    assert t_astropy == t_yt
+    assert t_float == t_yt
+    assert t_tuple == t_yt
