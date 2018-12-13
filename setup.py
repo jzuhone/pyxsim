560b93ac098002058140172a0dc40c2869a5e72b
John ZuHone <jzuhone@gmail.com>
2018-12-12 21:41:30 -0500

    Fix python versions here

--- setup.py
+++ setup.py
@@ -25,9 +25,9 @@ setup(name='pyxsim',
       classifiers=[
           'Intended Audience :: Science/Research',
           'Operating System :: OS Independent',
-          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Topic :: Scientific/Engineering :: Visualization',
       ],
       )
