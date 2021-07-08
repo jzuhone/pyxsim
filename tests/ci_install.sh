9ec5839f58183afed6683050f2803cff77ec0c3f
John ZuHone <jzuhone@gmail.com>
2021-07-07 22:22:39 -0400

    Start to use github actions

---
+++ tests/ci_install.sh
@@ -0,0 +1,21 @@
+set -x   # Show which command is being run
+
+# Download test data
+
+wget http://yt-project.org/data/enzo_tiny_cosmology.tar.gz
+tar -zxvf enzo_tiny_cosmology.tar.gz
+wget http://yt-project.org/data/GasSloshingLowRes.tar.gz
+tar -zxvf GasSloshingLowRes.tar.gz
+wget http://yt-project.org/data/FIRE_M12i_ref11.tar.gz
+tar -zxvf FIRE_M12i_ref11.tar.gz
+
+# Download answers
+
+wget http://hea-www.cfa.harvard.edu/~jzuhone/${ANSWER_VER}.tar.gz
+tar -zxvf ${ANSWER_VER}.tar.gz		
+
+# Set location of yt test data
+mkdir -p $HOME/.config/yt
+echo "[yt]" > $HOME/.config/yt/yt.toml
+echo "test_data_dir = ${PWD}" >> $HOME/.config/yt/yt.toml
+cat $HOME/.config/yt/yt.toml
