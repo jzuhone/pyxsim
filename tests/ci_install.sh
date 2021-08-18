c8410b619445cc5cd7e6f3a79a3e7ff4f509bc9c
John ZuHone <jzuhone@gmail.com>
2021-08-18 09:23:38 -0400

    Fixing things up

--- tests/ci_install.sh
+++ tests/ci_install.sh
@@ -1,24 +1,27 @@
 set -x   # Show which command is being run
 
-# Download test data
+if [[ ${mode} == "testing" ]]; then
 
-wget -q http://yt-project.org/data/enzo_tiny_cosmology.tar.gz
-tar -zxf enzo_tiny_cosmology.tar.gz
-wget -q http://yt-project.org/data/GasSloshingLowRes.tar.gz
-tar -zxf GasSloshingLowRes.tar.gz
-wget -q http://yt-project.org/data/FIRE_M12i_ref11.tar.gz
-tar -zxf FIRE_M12i_ref11.tar.gz
-
-# Download answers
-
-wget -q http://hea-www.cfa.harvard.edu/~jzuhone/${ANSWER_VER}.tar.gz
-tar -zxf ${ANSWER_VER}.tar.gz		
-
-# Set location of yt test data
-mkdir -p $HOME/.config/yt
-echo "[yt]" > $HOME/.config/yt/yt.toml
-echo "test_data_dir = \"${PWD}\"" >> $HOME/.config/yt/yt.toml
-cat $HOME/.config/yt/yt.toml
+    # Download test data
+    
+    wget -q http://yt-project.org/data/enzo_tiny_cosmology.tar.gz
+    tar -zxf enzo_tiny_cosmology.tar.gz
+    wget -q http://yt-project.org/data/GasSloshingLowRes.tar.gz
+    tar -zxf GasSloshingLowRes.tar.gz
+    wget -q http://yt-project.org/data/FIRE_M12i_ref11.tar.gz
+    tar -zxf FIRE_M12i_ref11.tar.gz
+    
+    # Download answers
+    
+    wget -q http://hea-www.cfa.harvard.edu/~jzuhone/${ANSWER_VER}.tar.gz
+    tar -zxf ${ANSWER_VER}.tar.gz		
+    
+    # Set location of yt test data
+    mkdir -p $HOME/.config/yt
+    echo "[yt]" > $HOME/.config/yt/yt.toml
+    echo "test_data_dir = \"${PWD}\"" >> $HOME/.config/yt/yt.toml
+    cat $HOME/.config/yt/yt.toml
+fi 
 
 # Install dependencies using conda
 
