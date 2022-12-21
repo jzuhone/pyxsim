2bea16848a8edcf024b86faf21ea73fe3ac45616
John ZuHone <jzuhone@gmail.com>
2022-12-21 18:20:00 -0500

    let-r rip

--- tests/ci_install.sh
+++ tests/ci_install.sh
@@ -4,16 +4,16 @@ if [[ ${mode} == "testing" ]]; then
 
     # Download test data
     
-    wget -q http://yt-project.org/data/enzo_tiny_cosmology.tar.gz
+    curl -O http://yt-project.org/data/enzo_tiny_cosmology.tar.gz
     tar -zxf enzo_tiny_cosmology.tar.gz
-    wget -q http://yt-project.org/data/GasSloshingLowRes.tar.gz
+    curl -O http://yt-project.org/data/GasSloshingLowRes.tar.gz
     tar -zxf GasSloshingLowRes.tar.gz
-    wget -q http://yt-project.org/data/FIRE_M12i_ref11.tar.gz
+    curl -O http://yt-project.org/data/FIRE_M12i_ref11.tar.gz
     tar -zxf FIRE_M12i_ref11.tar.gz
     
     # Download answers
     
-    wget -q http://hea-www.cfa.harvard.edu/~jzuhone/${ANSWER_VER}.tar.gz
+    curl -O http://hea-www.cfa.harvard.edu/~jzuhone/${ANSWER_VER}.tar.gz
     tar -zxf ${ANSWER_VER}.tar.gz		
     
     # Set location of yt test data
