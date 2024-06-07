3afa326ac717afb38baafc73cc87db81261ad062
John ZuHone <jzuhone@gmail.com>
2024-06-06 22:36:14 -0400

    Missed this

--- pyxsim/source_models/thermal_sources.py
+++ pyxsim/source_models/thermal_sources.py
@@ -1154,4 +1154,5 @@ class CXSourceModel(ThermalSourceModel):
         strs["collision_field"] = self.collision_field
         strs["collntype"] = self.collntype
         strs["acx_model"] = self.acx_model
+        strs["recomb_type"] = self.recomb_type
         return class_name, strs
