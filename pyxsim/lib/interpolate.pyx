4b9b698393c57d1d71cda1f77392b7a872c5c4a5
John ZuHone <jzuhone@gmail.com>
2024-07-17 10:26:07 -0400

    More CX

--- pyxsim/lib/interpolate.pyx
+++ pyxsim/lib/interpolate.pyx
@@ -141,3 +141,42 @@ def interp2d_spec(np.ndarray[np.float64_t, ndim=2] ctable,
         return coutput, moutput, voutput
     else:
         return coutput, moutput, None
+
+
+@cython.cdivision(True)
+@cython.wraparound(False)
+@cython.boundscheck(False)
+def interp_cx_spec(np.ndarray[np.float64_t, ndim=3] h_table,
+                   np.ndarray[np.float64_t, ndim=3] he_table,
+                   np.ndarray[np.float64_t, ndim=1] x_vals,
+                   np.ndarray[np.float64_t, ndim=1] x_bins,
+                   np.ndarray[np.int32_t, ndim=1] x_is):
+    cdef double x, dx_inv
+    cdef int i, x_i, j, k, nelem
+    cdef int nt = x_vals.shape[0]
+    cdef int ne = h_table.shape[2]
+    cdef np.ndarray[np.float64_t, ndim=1] xp, xm
+    cdef np.ndarray[np.float64_t, ndim=3] h_output, he_output
+
+    nelem = <int>h_table.shape[0]
+    voutput = np.zeros((nelem, nt, ne))
+
+    xp = np.zeros(nt)
+    xm = np.zeros(nt)
+
+    for i in range(nt):
+        x_i = x_is[i]
+        x = x_vals[i]
+        dx_inv = 1.0 / (x_bins[x_i+1] - x_bins[x_i])
+        xp[i] = (x - x_bins[x_i]) * dx_inv
+        xm[i] = (x_bins[x_i+1] - x) * dx_inv
+    for i in range(nt):
+        x_i = x_is[i]
+        for k in range(nelem):
+            for j in range(ne):
+                h_output[k, i, j] = h_table[k, x_i, j] * xm[i] + \
+                                    h_table[k, x_i + 1, j] * xp[i]
+                he_output[k, i, j] = he_table[k, x_i, j] * xm[i] + \
+                                     he_table[k, x_i + 1, j] * xp[i]
+
+    return h_output, he_output
