3572a688d96a344e2ff84e2309ab00f1eab8880e
John ZuHone <jzuhone@gmail.com>
2025-02-25 13:29:14 -0700

    Actually enable doppler shifting for power-law spectra

--- pyxsim/source_models/power_law_sources.py
+++ pyxsim/source_models/power_law_sources.py
@@ -7,7 +7,7 @@ from yt.data_objects.static_output import Dataset
 
 from pyxsim.lib.spectra import power_law_spectrum
 from pyxsim.source_models.sources import SourceModel
-from pyxsim.utils import check_num_cells, mylog, parse_value
+from pyxsim.utils import check_num_cells, mylog, parse_value, sanitize_normal
 
 
 class PowerLawSourceModel(SourceModel):
@@ -91,10 +91,18 @@ class PowerLawSourceModel(SourceModel):
             self.pbar.close()
 
     def make_spectrum(
-        self, data_source, emin, emax, nbins, redshift=0.0, dist=None, cosmology=None
+        self,
+        data_source,
+        emin,
+        emax,
+        nbins,
+        redshift=0.0,
+        dist=None,
+        cosmology=None,
+        normal=None,
     ):
         """
-        Make a count rate spectrum in the source frame from a yt data container,
+        Using all the data in a yt data container, make a count rate spectrum in the source frame,
         or a spectrum in the observer frame.
 
         Parameters
@@ -119,19 +127,34 @@ class PowerLawSourceModel(SourceModel):
             Cosmological information. If not supplied, we try to get the
             cosmology from the dataset. Otherwise, LCDM with the default yt
             parameters is assumed.
+        normal : integer, string, or array-like, optional
+            This is a line-of-sight direction along which the spectrum will be
+            Doppler shifted using the velocity field in the object. This is
+            only an option if the spectrum is calculated in the observer frame.
+            Options are one of "x", "y", "z", 0, 1, 2, or an 3-element array-like
+            object of floats to specify an off-axis normal vector.
 
         Returns
         -------
         :class:`~soxs.spectra.CountRateSpectrum` or :class:`~soxs.spectra.Spectrum`,
         depending on how the method is invoked.
         """
+        normal = sanitize_normal(normal)
+        if normal is not None:
+            if redshift == 0.0 and dist is None:
+                raise RuntimeError(
+                    "Cannot use a normal vector for the line-of-sight "
+                    "when a redshift or the distance is not specified! not specified or the distance "
+                )
+            data_source.set_field_parameter("axis", normal)
+        shifting = normal is not None
         ebins = np.linspace(emin, emax, nbins + 1)
         spec = np.zeros(nbins)
         spectral_norm = 1.0
         self.setup_model("spectrum", data_source, redshift)
         for chunk in data_source.chunks([], "io"):
             chunk_data = self.process_data(
-                "spectrum", chunk, spectral_norm, ebins=ebins
+                "spectrum", chunk, spectral_norm, shifting=shifting, ebins=ebins
             )
             if chunk_data is not None:
                 spec += chunk_data
